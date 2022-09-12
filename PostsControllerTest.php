5e398c3d4abc1361649e0c17f284bfb09cbce252
Tommy OnyxOne <tommy@onyxonlinelearning.eu>
2022-09-12 22:38:31 +0000

    (feat) add tests

--- PostsControllerTest.php
+++ PostsControllerTest.php
@@ -1,22 +1,21 @@
 <?php
+declare(strict_types=1);
 
 namespace Tmy\Test;
 
-use PostsController;
 use PHPUnit\Framework\TestCase;
 
 class PostsControllerTest extends TestCase
 {
-
     /**
      * @return void
      */
     public function testView()
     {
+        $this->assertEquals(1, 1, 'hi');
     }
 
     public function testAdd()
     {
-
     }
 }
