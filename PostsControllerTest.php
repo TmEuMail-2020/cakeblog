ae0f66df1ec806517564282bbdcb7b95af4e5720
Tommy OnyxOne <tommy@onyxonlinelearning.eu>
2022-09-12 22:21:16 +0000

    (WIP) try adding test

---
+++ PostsControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tmy\Test;
+
+use PostsController;
+use PHPUnit\Framework\TestCase;
+
+class PostsControllerTest extends TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function testView()
+    {
+    }
+
+    public function testAdd()
+    {
+
+    }
+}
